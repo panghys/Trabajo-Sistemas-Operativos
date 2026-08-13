705ce380d2a5046e95087f8df2a595931561b1cd
panghys <rbgm248@gmail.com>
2026-08-13 10:35:10 -0400

    plantilla del proyecto

---
+++ include/menu.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
---
+++ src/menu/menu.cpp
@@ -0,0 +1,6 @@
+#include "../include/menu.h"
+
+int main(){
+
+    return 0;
+}
